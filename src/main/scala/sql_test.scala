a6329af34b985065ff128042210aa52ee8051e4c
Ravi Chiripurapu <ravi.chiripurapu@toyota.com>
2021-01-06 17:15:43 -0600

    fixed column split

--- src/main/scala/sql_test.scala
+++ src/main/scala/sql_test.scala
@@ -20,30 +20,40 @@ object SQLTest extends App with Logging {
   import spark.implicits._
 
   // load customer data
-  val dfcustomer = spark.read.format("csv").option("header","true").option("inferSchema","true").load("./data/customer/*")
+  val dfcustomer = spark
+    .read
+    .format("csv")
+    .option("header","true")
+    .option("inferSchema","true")
+    .load("./data/customer/*")
   dfcustomer.show(false)
   dfcustomer.printSchema()
 
   // load sales data & add new column created_ts which is casted to timestamp
 
-  // val dfsales1 = spark.read.format("csv").option("header","true").option("inferSchema","true").load("./data/sales/*")
-  //  .selectExpr("*", "cast(sales_ts as timestamp) created_ts" )
+  /*
+  val dfsales1 = spark.read.format("csv").option("header","true").option("inferSchema","true").load("./data/sales/*")
+   */
+    .selectExpr("*", "cast(sales_ts as timestamp) created_ts" )
+    .selectExpr("*", "year(created_ts) year", "month(created_ts) month", "day(created_ts) day", "hour(created_ts) hour")
+  dfsales1.show(false)
+  dfsales1.printSchema()
+  */
 
-
-  // add additional columns in the data frame
-  // val dfsalesadditional = dfsales1.selectExpr("*", "year(created_ts) year", "month(created_ts) month", "day(created_ts) day", "hour(created_ts) hour")
-  // dfsalesadditional.show(false)
-  // dfsalesadditional.printSchema()
-
-
-  // build dfsales with builder pattern in a single statement.
-  var dfsales = spark
+  // build dfsales .
+  val dfsales = spark
     .read
     .format("csv")
     .option("header","true")
     .option("inferSchema","true")
     .load("./data/sales/*")
     .withColumn("created_ts", col("sales_ts").cast("timestamp") )
+    .withColumn("created_dt", split(col("created_ts")," ").getItem(0))
+    .withColumn("created_time", split(col("created_ts")," ").getItem(1))
+    .withColumn("year", split(col("created_dt"),"-").getItem(0))
+    .withColumn("month", split(col("created_dt"),"-").getItem(1))
+    .withColumn("day", split(col("created_dt"),"-").getItem(2))
+    .withColumn("hour", split(col("created_time"),":").getItem(0))
     //.withColumn("year1", year(col("created_ts")))
     //.withColumn("month1", month(col("created_ts")))
     //.withColumn("day1", dayofmonth(col("created_ts")))
@@ -51,34 +61,16 @@ object SQLTest extends App with Logging {
   dfsales.show(false)
   dfsales.printSchema()
 
-  val split_col = split(dfsales("created_ts")," ")
-
-  dfsales = dfsales
-    .withColumn("created_dt", split_col.getItem(0))
-    .withColumn("created_time", split_col.getItem(1))
-
-  val split_dt = split(dfsales("created_dt"),"-")
-
-  dfsales = dfsales
-    .withColumn("year", split_dt.getItem(0))
-    .withColumn("month", split_dt.getItem(1))
-    .withColumn("day", split_dt.getItem(2))
-
-  val split_ts = split(dfsales("created_time"),":")
-
-  dfsales = dfsales
-    .withColumn("hour", split_ts.getItem(0))
-
   // create a new dataframe by joining two datasets on column (dfcustomer.id and dfsalesadditional.customer_id
   val dfjoin = dfcustomer.join(dfsales,dfcustomer("id")===dfsales("customer_id"))
   dfjoin.show()
 
-  /*
-  dfjoin.createOrReplaceTempView("customer_sales")
+
+  /*dfjoin.createOrReplaceTempView("customer_sales")
   spark.sql("select state, year, month, day, hour, sum(sales_amount) from customer_sales group by state, year, month, day, hour " +
     "WITH ROLLUP order by state asc nulls last, year desc nulls last, month desc nulls last, day asc nulls last, hour desc nulls last")
-    .show(false)
-   */
+    .show(false)*/
+
 
   // create a new dataframe by applying rollup, agg, sort
   val withRollup = dfjoin
